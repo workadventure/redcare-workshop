e0c4f2aeec2681101bd275ba717edc4f29a3891d
Valdo <vteixeiraromao@gmail.com>
2024-07-01 17:50:25 +0100

    Fix script

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    // Show configuration tile for editors only
-    if (WA.player.tags.includes('editor')) {
-        WA.room.showLayer('exitNorthConfig')
-        WA.room.showLayer('exitSouthConfig')
-        WA.room.showLayer('exitWestConfig')
-        WA.room.showLayer('exitEastConfig')
-    }
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-}).catch(e => console.error(e));
-
 let currentZone: string;
 let currentPopup: any;
 
@@ -55,17 +36,35 @@ const config = [
     },
 ]
 
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
 
-WA.onEnterZone('needHelp', () => {
-    currentZone = 'needHelp'
-    openPopup(currentZone, currentZone + 'Popup')
-});
-WA.onEnterZone('followUs', () => {
-    currentZone = 'followUs'
-    openPopup(currentZone, currentZone + 'Popup')
-});
-WA.onLeaveZone('needHelp', closePopup);
-WA.onLeaveZone('followUs', closePopup);
+    // Show configuration tile for editors only
+    if (WA.player.tags.includes('editor')) {
+        WA.room.showLayer('exitNorthConfig')
+        WA.room.showLayer('exitSouthConfig')
+        WA.room.showLayer('exitWestConfig')
+        WA.room.showLayer('exitEastConfig')
+    }
+
+    WA.onEnterZone('needHelp', () => {
+        currentZone = 'needHelp'
+        openPopup(currentZone, currentZone + 'Popup')
+    });
+    WA.onEnterZone('followUs', () => {
+        currentZone = 'followUs'
+        openPopup(currentZone, currentZone + 'Popup')
+    });
+    WA.onLeaveZone('needHelp', closePopup);
+    WA.onLeaveZone('followUs', closePopup);
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+}).catch(e => console.error(e));
 
 
 function openPopup(zoneName: string, popupName: string) {
@@ -77,6 +76,7 @@ function openPopup(zoneName: string, popupName: string) {
         currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
     }
 }
+
 function closePopup(){
     if (typeof currentPopup !== undefined) {
         currentPopup.close();
